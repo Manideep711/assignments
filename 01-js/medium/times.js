dd205d97904a46bc078f23223b4a4856668f2ed2
Manideep kakkerla <manideepkakkerla2004@gmail.com>
2023-12-13 12:17:14 +0530

    completed time

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -7,7 +7,22 @@ Try running it for
 Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
-
-function calculateTime(n) {
-    return 0.01;
-}
+function calculateSumTime(n) {
+    let startTime = new Date().getTime(); // Get current time in milliseconds
+  
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+      sum += i;
+    }
+  
+    let endTime = new Date().getTime(); // Get current time again
+    let timeTaken = (endTime - startTime) / 1000; // Calculate time difference in seconds
+  
+    return timeTaken;
+  }
+  
+  // Test cases
+  console.log("Time taken to calculate sum from 1 to 100:", calculateSumTime(100), "seconds");
+  console.log("Time taken to calculate sum from 1 to 100000:", calculateSumTime(100000), "seconds");
+  console.log("Time taken to calculate sum from 1 to 1000000000:", calculateSumTime(1000000000), "seconds");
+  
