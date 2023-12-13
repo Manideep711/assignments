3c6a34ae0b942a27e424b1811f35c1b7baeb1570
Manideep kakkerla <manideepkakkerla2004@gmail.com>
2023-12-13 12:06:57 +0530

    completed largest element

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,13 @@
 */
 
 function findLargestElement(numbers) {
-    
+    let greatest=numbers[0];
+    for(let i=0;i<=numbers.length;i++){
+        if(numbers[i]>greatest){
+            greatest=numbers[i]
+        }
+    }
+    return greatest
 }
 
 module.exports = findLargestElement;
