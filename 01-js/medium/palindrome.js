9455c9d3eaebc4ef9d3274786f1cb8a99c99d44e
Manideep kakkerla <manideepkakkerla2004@gmail.com>
2023-12-13 12:35:05 +0530

    completed palindrome

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,20 @@
 */
 
 function isPalindrome(str) {
-  return true;
-}
+  // const cleanStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+
+  // // Reversing the cleaned string
+  // const reversedStr = cleanStr.split('').reverse().join('');
+
+  // // Check if the cleaned string is equal to its reverse
+  // return cleanStr === reversedStr;
+    let reversed="";
+   let cleanstr=str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    for (let i=cleanstr.length-1; i>=0; i--){
+  reversed+=cleanstr[i];
+    }
+    return cleanstr===reversed;
+  }
+  
 
 module.exports = isPalindrome;
